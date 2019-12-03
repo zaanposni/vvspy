b6a70d5927658f6101f98eb3b540b9b76e43fb4f
zaanposni <zaanposni@users.noreply.github.com>
2019-12-03 10:59:26 +0100

    minor refactors

--- vvspy/__init__.py
+++ vvspy/__init__.py
@@ -1,9 +1,13 @@
 from datetime import datetime as __datetime
+from typing import List, Union
 
+from .obj import Arrival, Departure
 from .trip import get_trip
 from .departures import departures
 
 
-def departure_now(station_id: str, limit: int = 100, request_params: dict = None, **kwargs):
-    departures(station_id=station_id, check_time=__datetime.now(), limit=limit, config=kwargs,
-               request_params=request_params)
+def departure_now(station_id: Union[str, int], limit: int = 100, request_params: dict = None,
+                  **kwargs) -> List[Union[Arrival, Departure]]:
+    return departures(station_id=station_id, check_time=__datetime.now(), limit=limit, request_params=request_params,
+                      **kwargs)
+
