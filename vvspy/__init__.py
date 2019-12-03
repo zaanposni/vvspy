1e0dd370c2bb7349a962f2463cf4ea2e2c33f560
zaanposni <Yannick.Gross@de.bosch.com>
2019-12-03 10:00:51 +0100

    easy2use function

--- vvspy/__init__.py
+++ vvspy/__init__.py
@@ -1,2 +1,9 @@
-from .trip import gettrip
+from datetime import datetime as __datetime
+
+from .trip import get_trip
 from .departures import departures
+
+
+def departure_now(station_id: str, limit: int = 100, request_params: dict = None, **kwargs):
+    departures(station_id=station_id, check_time=__datetime.now(), limit=limit, config=kwargs,
+               request_params=request_params)
