68a42e0d3b6dbf96b32c1be135b2a0fdf8a232a0
zaanposni <zaanposni@users.noreply.github.com>
2019-12-08 16:17:34 +0100

    basic structure for unittests

---
+++ tests/start.py
@@ -0,0 +1,9 @@
+import unittest
+
+from unittests import TestParser as test_a
+
+# TODO: import new unittests in alphabetical order
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/unittests/parser.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class TestParser(unittest.TestCase):
+    def test_start(self):
+        # ===========================================================================
+        print("Testing stuff")
+        self.assertTrue(True)
