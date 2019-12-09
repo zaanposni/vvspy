f183544a164760c24560fb37989707f609c60176
zaanposni <zaanposni@users.noreply.github.com>
2019-12-09 18:55:17 +0100

    added unittest on object creation

--- tests/start.py
+++ tests/start.py
@@ -4,6 +4,9 @@ from unittests import TestParser as test_a
 
 # TODO: import new unittests in alphabetical order
 
+tests = [test_a]
 
 if __name__ == '__main__':
+    print(f"Tests: {[x.__name__ for x in tests]}")
+    print("================================================\n")
     unittest.main()
--- tests/unittests/parser.py
+++ tests/unittests/parser.py
@@ -1,8 +1,78 @@
 import unittest
+import sys
+import os.path
+import json
+
+sys.path.append("..")
+
+from vvspy.trip import _parse_response as parse_trip_response
+from vvspy.arrivals import _parse_response as parse_arrival_response
+from vvspy.departures import _parse_response as parse_departure_response
+from vvspy.obj import Trip
+from vvspy.obj import Arrival
+from vvspy.obj import Departure
+
+path = os.path.join(".", "mock_results")
 
 
 class TestParser(unittest.TestCase):
+    def trip_200(self):
+        with open(os.path.join(path, "trip_request_200.json"), 'r') as f:
+            data = json.load(f)
+        results = parse_trip_response(data)
+        self.assertTrue(all([isinstance(x, Trip) for x in results]))
+
+    def trip_400(self):
+        with open(os.path.join(path, "trip_request_400.json"), 'r') as f:
+            data = json.load(f)
+        results = parse_trip_response(data)
+        self.assertEqual(results, [])
+
+    def arrival_200(self):
+        with open(os.path.join(path, "arrival_request_200.json"), 'r') as f:
+            data = json.load(f)
+        results = parse_arrival_response(data)
+        self.assertTrue(all([isinstance(x, Arrival) for x in results]))
+
+    def arrival_400(self):
+        with open(os.path.join(path, "arrival_request_400.json"), 'r') as f:
+            data = json.load(f)
+        results = parse_arrival_response(data)
+        self.assertEqual(results, [])
+
+    def departure_200(self):
+        with open(os.path.join(path, "departure_request_200.json"), 'r') as f:
+            data = json.load(f)
+        results = parse_arrival_response(data)
+        self.assertTrue(all([isinstance(x, Departure) for x in results]))
+
+    def departure_400(self):
+        with open(os.path.join(path, "departure_request_400.json"), 'r') as f:
+            data = json.load(f)
+        results = parse_arrival_response(data)
+        self.assertEqual(results, [])
+
     def test_start(self):
         # ===========================================================================
-        print("Testing stuff")
-        self.assertTrue(True)
+        print("Test trip parser (200 mock result)")
+        self.trip_200()
+
+        # ===========================================================================
+        print("Test trip parser (400 mock result)")
+        self.trip_400()
+
+        # ===========================================================================
+        print("Test arrival parser (200 mock result)")
+        self.arrival_200()
+
+        # ===========================================================================
+        print("Test arrival parser (400 mock result)")
+        self.arrival_400()
+
+        # ===========================================================================
+        print("Test departure parser (200 mock result)")
+        self.departure_200()
+
+        # ===========================================================================
+        print("Test departure parser (400 mock result)")
+        self.departure_400()
