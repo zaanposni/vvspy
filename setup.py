b872e6660b85f3090eb31c21b2ff78dbff01a48e
zaanposni <zaanposni@users.noreply.github.com>
2019-12-31 00:34:59 +0100

    bumped version to 1.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
   name='vvspy',
   py_modules=["vvspy"],
-  version='1.1.0',
+  version='1.1.1',
   license='MIT',
   description='API Wrapper for VVS (Verkehrsverbund Stuttgart)',
   author='zaanposni',
