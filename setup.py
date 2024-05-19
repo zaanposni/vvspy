857a29cb4f72ebb7ce4ae4d1b68e0cd6cc4ce7db
zaanposni <me@zaanposni.com>
2024-05-19 16:10:29 +0200

    correctly add all available python versions

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
     ],
     long_description=long_description,
