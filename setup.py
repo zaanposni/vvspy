5b39d35c4b5e1a73072c8a42de8162ae459882b5
zaanposni <me@zaanposni.com>
2024-05-21 20:53:52 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setup(
     name="vvspy",
-    version="2.0.2",
+    version="2.1.0",
     license="MIT",
     description="API Wrapper for VVS (Verkehrsverbund Stuttgart)",
     author="zaanposni",
